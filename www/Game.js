11b24d3b4b4ea04d328a01f6bcf4c0345a55ac9c
jaztothemin <jazivarasak@gmail.com>
2020-04-16 13:40:24 +0200

    ChangesInGamejs

--- www/Game.js
+++ www/Game.js
@@ -1,4 +1,12 @@
-class Game { }
+class Game {
+
+  constructor(addEventListener, Start)
+
+  start()
+
+  tellTurn()
+
+}
 
 // make it possible to test on backend
 if (typeof global !== 'undefined') { global.Game = Game };
