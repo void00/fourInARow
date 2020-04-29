a458648fb2d21077bbdede65459116d1237f524d
jaztothemin <jazivarasak@gmail.com>
2020-04-29 17:30:30 +0200

    Added code in step def-Board

--- step-definitions/board-test.js
+++ step-definitions/board-test.js
@@ -5,4 +5,17 @@ module.exports = function () {
   let game;
 
 }
+this.Given(/^ Game is created $/, function () {
+  game = new Game();
+  board = new board();
+});
 
+this.Then(/^Game should be an instance of game$/, function () {
+  expect(board.game).to.be.an.instanceof(Game)
+});
+
+this.Then(/^If game is not an instance of Game, it should give an error$/, function (expectError) {
+  expect(() => new board({})).to.throw(
+    Error, expectedError,
+  );
+});
