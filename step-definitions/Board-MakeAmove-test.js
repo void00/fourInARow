b3e8cc2dce252401b6e4f9517bef3310b78984ce
jaztothemin <jazivarasak@gmail.com>
2020-05-02 17:39:25 +0200

    Added FakeGame and FakeBoard in MakeAmove

--- step-definitions/Board-MakeAmove-test.js
+++ step-definitions/Board-MakeAmove-test.js
@@ -3,6 +3,23 @@ require('./_async-helpers.js');
 
 module.exports = function () {
 }
+
+class FakeBoard extends Board {
+  wincheck() {
+
+  }
+}
+
+
+class FakeGame extends Game {
+  start() { this.board = new FakeBoard(this); }
+}
+
+let fakeGame = new FakeGame();
+
+
+
+
 this.Given(/^that method returns null when PlayInProgress is set to true$/, function () {
   expect(fakeGame.board.PlayInProgress).to.be.true;
 });
