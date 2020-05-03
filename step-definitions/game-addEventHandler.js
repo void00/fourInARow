3d929343ee35b71a417fe0a630d42ce0e63272ff
jaztothemin <jazivarasak@gmail.com>
2020-05-03 19:27:18 +0200

    small changes renaming

--- step-definitions/game-addEventHandler.js
+++ step-definitions/game-addEventHandler.js
@@ -2,7 +2,7 @@ require('./_include-all')();
 
 module.exports = function () {
 
-  //Scenario: The game should add an reset button when won or draw is made
+  //Scenario: The game should add a reset button when won or draw is made
   this.Given(/^the game is over$/, function () {
 
   });
