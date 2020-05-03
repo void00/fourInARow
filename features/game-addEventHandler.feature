dfe60b244b4538602a2e243b3c4f0cad7b8b7d74
jaztothemin <jazivarasak@gmail.com>
2020-05-03 21:35:52 +0200

    addEventHandler test and coding

--- features/game-addEventHandler.feature
+++ features/game-addEventHandler.feature
@@ -1,7 +1,7 @@
 Feature:The game should have a button for play again
   So we can restart
 
-  Scenario:The game should add an reset button when won or draw is made
+  Scenario:The game should add a reset button when won or draw is made
     Given the game is over
     Then there should be a button with an eventListener added in element message
     And when the button is clicked start should be called
--- step-definitions/board-MakeMove-test.js
+++ step-definitions/board-MakeMove-test.js
@@ -2,29 +2,29 @@ require('./_include-all')();
 require('./_async-helpers.js');
 
 module.exports = function () {
-}
 
 
 
-class FakeBoard extends Board {
-  wincheck() {
-    this.winCheckCalled = true;
+
+  class FakeBoard extends Board {
+    wincheck() {
+      this.winCheckCalled = true;
+    }
+    render() {
+      this.renderWasCalled = true;
+    }
   }
-  render() {
-    this.renderWasCalled = true;
+
+
+
+  class FakeGame extends Game {
+    start() { this.board = new FakeBoard(this); }
+    tellTurn(player) {
+      //tellTurnPlayer.push(player);
+    }
   }
+
+  let fakeGame = new FakeGame();
+
 }
-
-
-
-class FakeGame extends Game {
-  start() { this.board = new FakeBoard(this); }
-  tellTurn(player) {
-    //tellTurnPlayer.push(player);
-  }
-}
-
-let fakeGame = new FakeGame();
-
-
 
